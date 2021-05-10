001c8b9a32f5b14c2c31cf3ea6989494edf2a8b5
Tai Nguyen Trong <dongthoigian37@gmail.com>
2021-05-10 22:12:16 +0700

    Integrate with BE

--- server/app_collab.py
+++ server/app_collab.py
@@ -1,17 +1,353 @@
-import os
-from flask import Flask, flash, request, redirect, url_for, send_file, session
-from flask_cors import CORS, cross_origin
+from __future__ import division
 from flask_ngrok import run_with_ngrok
+from flask_cors import CORS, cross_origin
+from flask import Flask, flash, request, redirect, url_for, send_file, session
+import time
+import torch
+import copy
+from torch.autograd import Variable
+import numpy as np
+import cv2
+from util import *
+from darknet import Darknet
+from preprocess import prep_image, inp_to_image, letterbox_image
+import pandas as pd
+import random
+import pickle as pkl
+import argparse
+from deep_sort import DeepSort
+from collections import deque
+from backbone.base import Base as BackboneBase
+from config.train_config import TrainConfig
+from config.eval_config import EvalConfig
+from config.config import Config
+from model import Model
+import csv
+import pandas as pd
+import os
 
-UPLOAD_FOLDER = '/uploads'
-ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 
-model_types_to_fn = {
-    "cycle_gan": CycleGAN,
-    "unit": CycleGAN,
-    "munit": CycleGAN,
-    "cut": CycleGAN,
-}
+def index2class():
+    file_path = '/content/SlowFast/ava/ava_action_list_v2.0.csv'
+    with open(file_path) as f:
+        i2c_dic = {line.split(',')[0]: line.split(',')[1] for line in f}
+    return i2c_dic
+
+
+def get_test_input(input_dim, CUDA):
+    img = cv2.imread("/content/SlowFast/imgs/dog.jpg")
+    img = cv2.resize(img, (input_dim, input_dim))
+    img_ = img[:, :, ::-1].transpose((2, 0, 1))
+    img_ = img_[np.newaxis, :, :, :]/255.0
+    img_ = torch.from_numpy(img_).float()
+    img_ = Variable(img_)
+
+    if CUDA:
+        img_ = img_.cuda()
+
+    return img_
+
+
+def prep_image(img, inp_dim):
+    """
+    Prepare image for inputting to the neural network.
+
+    Returns a Variable
+    """
+
+    orig_im = img
+    dim = orig_im.shape[1], orig_im.shape[0]
+    img = (letterbox_image(orig_im, (inp_dim, inp_dim)))
+    img_ = img[:, :, ::-1].transpose((2, 0, 1)).copy()
+    img_ = torch.from_numpy(img_).float().div(255.0).unsqueeze(0)
+    return img_, orig_im, dim
+
+
+def normalize(frame):
+    # Normalize the buffer
+    # buffer = (buffer - 128)/128.0
+    frame = (frame - np.array([[[128.0, 128.0, 128.0]]]))/128.0
+    return frame
+
+
+def to_tensor(buffer):
+    # convert from [D, H, W, C] format to [C, D, H, W] (what PyTorch uses)
+    # D = Depth (in this case, time), H = Height, W = Width, C = Channels
+    return buffer.transpose([3, 0, 1, 2])
+
+
+def imshow(video_name, frame, bboxes, labels, probs, ids, count):
+    for bbox, lables, prob, i in zip(bboxes, labels, probs, ids):
+        count_2 = 0
+        max_prob = max(prob)
+        for lable, p in zip(lables, prob):
+            if p <= max_prob:
+                continue
+            count_2 = count_2 + 1
+            bbox = np.array(bbox)
+            lable = int(lable)
+            p = float(p)
+            # rat = (ZZ,i, round(p, 2),index2class()[str(lable)].split("(")[0])
+            # para.append(rat)
+            real_x_min = int(bbox[0])
+            real_y_min = int(bbox[1])
+            real_x_max = int(bbox[2])
+            real_y_max = int(bbox[3])
+
+            # Draw rectangle on each frame, frame frame, (four coordinate parameters), (color), width
+            cv2.rectangle(frame, (real_x_min, real_y_min), (real_x_max,
+                          real_y_max), (255, 87, 51), 2)  # yellow color
+
+            cv2.putText(frame, index2class()[str(lable)].split("(")[0] + ':' + str(round(p, 2)),
+                        (real_x_min + 15, real_y_max - 15 * count_2),
+                        cv2.FONT_HERSHEY_COMPLEX,
+                        0.5, (0, 255, 195), 1, False)
+
+            cv2.putText(frame, "Person: " + str(i),
+                        (real_x_min + 10, real_y_min + 20),
+                        cv2.FONT_HERSHEY_COMPLEX,
+                        0.5, (0, 255, 195), 1, False)
+
+        cv2.imwrite(
+            f"demo/outputs/{video_name}/frames/%04d.jpg" % count, frame)
+
+
+def arg_parse():
+    """
+    Parse arguements to the detect module
+
+    """
+
+    parser = argparse.ArgumentParser(
+        description='YOLO v3 Video Detection Module')
+
+    # change the video path accordingly
+    parser.add_argument("--video", dest='video', help="Video to run detection upon",
+                        default="/content/SlowFast/new.mp4", type=str)
+    parser.add_argument("--dataset", dest="dataset",
+                        help="Dataset on which the network has been trained", default="pascal")
+    parser.add_argument("--confidence", dest="confidence",
+                        help="Object Confidence to filter predictions", default=0.5)
+    parser.add_argument("--nms_thresh", dest="nms_thresh",
+                        help="NMS Threshhold", default=0.4)
+
+    # change the path accordingly
+    parser.add_argument("--cfg", dest='cfgfile', help="Config file",
+                        default="/content/SlowFast/cfg/yolov3.cfg", type=str)
+
+    # change the path accordingly
+    parser.add_argument("--weights", dest='weightsfile', help="weightsfile",
+                        default="/content/SlowFast/yolov3.weights", type=str)
+    parser.add_argument("--reso", dest='reso', help="Input resolution of the network. Increase to increase accuracy. Decrease to increase speed",
+                        default="416", type=str)
+    return parser.parse_args()
+
+
+def SlowFast(model, CUDA, videofile, video_name):
+    # Video file on which you want to run the model
+    cap = cv2.VideoCapture(videofile)
+
+    assert cap.isOpened(), 'Cannot capture source'
+
+    frames = 0
+    last = np.array([])
+    last_time = time.time()
+
+    start = time.time()
+
+    #######for sp detec##########
+    buffer = deque(maxlen=64)
+    resize_width = 400
+    resize_height = 300
+
+    count = 0
+    while cap.isOpened():
+
+        ret, frame = cap.read()
+        if ret:
+
+            #######for sp detec##########
+            f = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            # will resize frames if not already final size
+            f = cv2.resize(frame, (resize_width, resize_height))
+            f = normalize(f)
+            buffer.append(f)
+            frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+            frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+            print(frame_height, frame_width)
+            scale = [resize_width / frame_width, resize_height / frame_height]
+
+            img, orig_im, dim = prep_image(frame, inp_dim)
+
+            im_dim = torch.FloatTensor(dim).repeat(1, 2)
+
+            if CUDA:
+                im_dim = im_dim.cuda()
+                img = img.cuda()
+
+            with torch.no_grad():
+                output = model(Variable(img), CUDA)
+            output = write_results(
+                output, confidence, num_classes, nms=True, nms_conf=nms_thesh)
+
+            if type(output) == int:
+                frames += 1
+                # ZZ+= 1
+                print("FPS of the video is {:5.2f}".format(
+                    frames / (time.time() - start)))
+                # print(ZZ)
+                #cv2.imshow("frame", orig_im)
+                key = cv2.waitKey(1)
+                if key & 0xFF == ord('q'):
+                    break
+                continue
+
+            im_dim = im_dim.repeat(output.size(0), 1)
+            scaling_factor = torch.min(inp_dim/im_dim, 1)[0].view(-1, 1)
+
+            output[:, [1, 3]] -= (inp_dim - scaling_factor *
+                                  im_dim[:, 0].view(-1, 1))/2
+            output[:, [2, 4]] -= (inp_dim - scaling_factor *
+                                  im_dim[:, 1].view(-1, 1))/2
+
+            output[:, 1:5] /= scaling_factor
+
+            for i in range(output.shape[0]):
+                output[i, [1, 3]] = torch.clamp(
+                    output[i, [1, 3]], 0.0, im_dim[i, 0])
+                output[i, [2, 4]] = torch.clamp(
+                    output[i, [2, 4]], 0.0, im_dim[i, 1])
+
+            output = output.cpu().data.numpy()
+            # print(output)
+            bbox_xywh = output[:, 1:5]
+            bbox_xywh[:, 2] = bbox_xywh[:, 2] - bbox_xywh[:, 0]
+            bbox_xywh[:, 3] = bbox_xywh[:, 3] - bbox_xywh[:, 1]
+
+            bbox_xywh[:, 0] = bbox_xywh[:, 0] + (bbox_xywh[:, 2])/2
+            bbox_xywh[:, 1] = bbox_xywh[:, 1] + (bbox_xywh[:, 3])/2
+            cls_conf = output[:, 5]
+            cls_ids = output[:, 7]
+
+            if bbox_xywh is not None:
+                mask = cls_ids == 0.0
+                bbox_xywh = bbox_xywh[mask]
+                cls_conf = cls_conf[mask]
+                outputs = deepsort.update(bbox_xywh, cls_conf, orig_im)
+
+                if len(outputs) > 0:
+                    bbox_xyxy = outputs[:, :4]
+                    identities = outputs[:, -1]
+
+            if len(buffer) == 64:
+                if count % 3 == 0:
+                    b = buffer
+                    a = time.time()
+                    b = np.array(b, dtype=np.float32)
+                    print("time:", time.time() - a)
+                    b = to_tensor(b)
+
+                    image_batch = torch.tensor(
+                        b, dtype=torch.float).unsqueeze(0).cuda()
+                    bbox_xyxy = np.array(bbox_xyxy, dtype=np.float)
+                    bbox_xyxy[:, [0, 2]] *= scale[0]
+                    bbox_xyxy[:, [1, 3]] *= scale[1]
+                    detector_bboxes = torch.tensor(
+                        bbox_xyxy, dtype=torch.float).unsqueeze(0).cuda()
+
+                    with torch.no_grad():
+                        detection_bboxes, detection_classes, detection_probs = \
+                            model_sf.eval().forward(image_batch, detector_bboxes_batch=detector_bboxes)
+
+                    detection_bboxes = np.array(detection_bboxes.cpu())
+                    detection_classes = np.array(detection_classes)
+                    detection_probs = np.array(detection_probs)
+                    # Get the corresponding classification label
+                    detection_bboxes[:, [0, 2]] /= scale[0]
+                    detection_bboxes[:, [1, 3]] /= scale[1]
+                imshow(video_name, frame, detection_bboxes,
+                       detection_classes, detection_probs, identities, count)
+                count += 1
+
+            #cv2.imshow("frame", orig_im)
+            key = cv2.waitKey(0)
+            if key & 0xFF == ord('q'):
+                break
+            frames += 1
+            # ZZ += 1
+            print("FPS of the video is {:5.2f}".format(
+                frames / (time.time() - start)))
+            # print(ZZ)
+
+        else:
+            break
+    convert_to_video = f"""ffmpeg -framerate 30 -pattern_type glob -i "demo/outputs/{video_name}/frames/*.jpg" -c:v libx264 -pix_fmt yuv420p demo/outputs/{video_name}/videos/video.mp4"""
+    os.system(convert_to_video)
+
+
+args = arg_parse()
+confidence = float("0.5")
+nms_thesh = float("0.4")
+start = 0
+
+CUDA = torch.cuda.is_available()
+
+num_classes = 80
+
+CUDA = torch.cuda.is_available()
+
+bbox_attrs = 5 + num_classes
+
+# change the path accordingly
+print("Loading network.....")
+model = Darknet("/content/SlowFast/cfg/yolov3.cfg")
+model.load_weights("/content/SlowFast/yolov3.weights")
+print("Network successfully loaded")
+
+# change the path accordingly
+print("load deep sort network....")
+deepsort = DeepSort("/content/SlowFast/deep/checkpoint/ckpt.t7")
+print("Deep Sort Network successfully loaded")
+
+model.net_info["height"] = args.reso
+inp_dim = int(model.net_info["height"])
+assert inp_dim % 32 == 0
+assert inp_dim > 32
+
+if CUDA:
+    model.cuda()
+
+
+# model(get_test_input(inp_dim, CUDA), CUDA)
+
+model.eval()
+
+#######for sp detec##########
+
+# Initialize the model and change the path for the weights accordingly
+path_to_checkpoint = "/content/SlowFast/slowfast_weight.pth"
+backbone_name = Config.BACKBONE_NAME
+backbone = BackboneBase.from_name(backbone_name)()
+model_sf = Model(backbone, 81, pooler_mode=Config.POOLER_MODE,
+                 anchor_ratios=Config.ANCHOR_RATIOS, anchor_sizes=Config.ANCHOR_SIZES,
+                 rpn_pre_nms_top_n=TrainConfig.RPN_PRE_NMS_TOP_N,
+                 rpn_post_nms_top_n=TrainConfig.RPN_POST_NMS_TOP_N).cuda()
+model_sf.load(path_to_checkpoint)
+
+
+video_path = "/content/SlowFast/demo/inputs/walk.mp4"
+video_name = "walk"
+os.system(f"mkdir -p demo/outputs/{video_name}/frames")
+os.system(f"mkdir -p demo/outputs/{video_name}/videos")
+SlowFast(model, CUDA, video_path, video_name)
+
+
+# -----------------------------------------------------
+# SERVER ----------------------------------------------
+# -----------------------------------------------------
+
+UPLOAD_FOLDER = '/content/SlowFast/demo/inputs/'
+ALLOWED_EXTENSIONS = {'mp4'}
 
 
 def allowed_file(filename):
@@ -19,12 +355,8 @@ def allowed_file(filename):
         filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
-def transform_image(image_path, model_type):
-    return model_types_to_fn.get(model_type, CycleGAN)(image_path)
-
-
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = os.getcwd() + UPLOAD_FOLDER
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 run_with_ngrok(app)
 
 
@@ -33,19 +365,21 @@ def test():
     return "Hello World!."
 
 
-@app.route("/api/transform-image", methods=["POST"])
+@app.route("/api/transform-video", methods=["POST"])
 @cross_origin(origin="*")
 def index():
-    image = request.files['file']
-    model_type = request.form.get("modelType")
-    if image and allowed_file(image.filename):
-        image_path = os.path.join(app.config['UPLOAD_FOLDER'], image.filename)
-        image.save(image_path)
-        image_output = transform_image(image_path, model_type)
-        image_output_path = os.path.join(
-            app.config['UPLOAD_FOLDER'], 'out.png')
-        image_output.save(image_output_path)
-        return send_file(image_output_path, mimetype='image/png')
+    video = request.files['file']
+    if video and allowed_file(video.filename):
+        video_path = os.path.join(app.config['UPLOAD_FOLDER'], video.filename)
+        video.save(image_path)
+        video_name = str(time.time())
+        os.system(f"mkdir -p demo/outputs/{video_name}/frames")
+        os.system(f"mkdir -p demo/outputs/{video_name}/videos")
+        SlowFast(model, CUDA, video_path, video_name)
+        video_output_path = f"demo/outputs/{video_name}/videos/video.mp4"
+        return {
+            "path": video_output_path,
+        }
 
 
 app.run()
--- src/Services/upload.js
+++ src/Services/upload.js
@@ -1,30 +1,17 @@
-// import axios from "axios";
+import axios from "axios";
 
-// const getNgrokEndpoint = () =>
-//   localStorage.getItem("ngrok-endpoint") || "http://f9770404ad74.ngrok.io";
+const getNgrokEndpoint = () =>
+  localStorage.getItem("ngrok-endpoint") || "http://ad3f8b71acba.ngrok.io";
 
-const uploadVideo = async () => {
-  // const UPLOAD_ENDPOINT = `${getNgrokEndpoint()}/api/transform-video`;
-  // const response = await axios.post(UPLOAD_ENDPOINT, formData, {
-  //   headers: {
-  //     "Content-Type": "multipart/form-data",
-  //   },
-  //   responseType: "arraybuffer",
-  // });
+const uploadVideo = async (formData) => {
+  const UPLOAD_ENDPOINT = `${getNgrokEndpoint()}/api/transform-video`;
+  const response = await axios.post(UPLOAD_ENDPOINT, formData, {
+    headers: {
+      "Content-Type": "multipart/form-data",
+    },
+  });
 
-  // const base64 = btoa(
-  //   new Uint8Array(response.data).reduce(
-  //     (data, byte) => data + String.fromCharCode(byte),
-  //     ""
-  //   )
-  // );
-
-  // return `data:${response.headers[
-  //   "content-type"
-  // ].toLowerCase()};base64,${base64}`;
-  return await new Promise((resolve) =>
-    setTimeout(() => resolve("http://localhost:5000/api/test"), 2000)
-  );
+  return response.data.path;
 };
 
-export { uploadVideo };
+export { uploadVideo, getNgrokEndpoint };
